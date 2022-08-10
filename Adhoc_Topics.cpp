6300bd9e2581e738fc158dc475d4c179a63296f5
Md. Parvej <62025097+Parvej23@users.noreply.github.com>
2022-08-10 13:50:16 +0600

    Add files via upload

---
+++ Adhoc_Topics.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    /*
+    Topics covers
+    __________
+    1. 1-D Prefix sums
+    2. Difference Array
+    3.2-D Prefix Sum
+    4. Find the sum of all subarray of an Array
+    5. Permutation
+    6. A score of a set is the difference between its maximum value
+    and minimum value. Find sum of scores over all subarrays.
+    */
+}
